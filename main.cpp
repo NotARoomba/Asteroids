02910f6ea2078e0c2a95705b706b57dbd27108f7
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2022-05-30 15:58:37 -0500

    better controls

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL.h>
 #include <math.h>
+#include <vector>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -10,7 +11,7 @@ struct {
 	float angle = 0;
 	float xVel = 0;
 	float yVel = 0;
-	float speed = .1;
+	float speed = .05;
 	
 } ship;
 SDL_FPoint rotateShip(int x, int y, int cx, int cy, int angle)
@@ -21,7 +22,7 @@ SDL_FPoint rotateShip(int x, int y, int cx, int cy, int angle)
 	return coords;
 };
 
-SDL_FPoint drawShip(SDL_Renderer* renderer, int x, int y, int rotation) {
+SDL_FPoint drawShip(SDL_Renderer* renderer, float x, float y, float rotation) {
 	SDL_FPoint points[5] = {
 		//tip
 		{x, y},
@@ -34,11 +35,9 @@ SDL_FPoint drawShip(SDL_Renderer* renderer, int x, int y, int rotation) {
 	  //tip
 	  {x, y}
 	};
-	//SDL_FPoint altPoints[5] = points;
+	//SDL_FPoint altPoints[5] = { points[0], points[1], points[2], points[3], points[4] };
 	for (int i = 0; i < 5; i++) {
 		//std::cout << points[i].x << " " << points[i].y << std::endl;
-
-		//if one of the points of the ship is outside of the screen make a copy of the ship and move it to the other side of the screen
 		if (points[i].x < 0.0f) {
 			points[i].x = (float)SCREEN_WIDTH + points[i].x;
 		}
@@ -54,7 +53,6 @@ SDL_FPoint drawShip(SDL_Renderer* renderer, int x, int y, int rotation) {
 	}
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
 	SDL_RenderDrawLinesF(renderer, points, 5);
-	//SDL_RenderDrawLinesF(renderer, altPoints, 5);
 	return points[0];
 };
 int main(int argc, char** argv)
@@ -71,21 +69,22 @@ int main(int argc, char** argv)
 		ship.yVel = ship.yVel > 1? 1 : ship.yVel < -1 ? -1 : ship.yVel;
 		ship.x += ship.xVel;
 		ship.y += ship.yVel;
+		const Uint8* keystates = SDL_GetKeyboardState(NULL);
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				done = true;
 			}
-			SDL_PumpEvents();
-			if (keys[SDL_SCANCODE_UP]) {
-				ship.yVel -= ship.speed * cos(ship.angle * M_PI / 180);
-				ship.xVel += ship.speed * sin(ship.angle * M_PI / 180);
-			}
-			if (keys[SDL_SCANCODE_LEFT]) {
-				ship.angle -= 8;
-			}
-			if (keys[SDL_SCANCODE_RIGHT]) {
-				ship.angle += 8;
-			}
+		}
+		SDL_PumpEvents();
+		if (keystates[SDL_SCANCODE_UP]) {
+			ship.yVel -= ship.speed * cos(ship.angle * M_PI / 180);
+			ship.xVel += ship.speed * sin(ship.angle * M_PI / 180);
+		}
+		if (keystates[SDL_SCANCODE_LEFT]) {
+			ship.angle -= 3;
+		}
+		if (keystates[SDL_SCANCODE_RIGHT]) {
+			ship.angle += 3;
 		}
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear(renderer);
