418ec32dc4d0e44802de879720cde1fb51c26649
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-08-09 19:16:14 -0500

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,26 @@
 import express, { Request, Response } from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import { connectToDatabase } from "./services/database.service";
 import { scoresRouter } from "./routers/scores.router";
 
 const app = express();
 const port = 3001;
 
+const corsOptions: CorsOptions = {
+  origin: ['https://asteroids.notaroomba.xyz', 'http://asteroids.notaroomba.xyz'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // enable set cookie
+}
+
 connectToDatabase()
   .then(() => {
-    app.use(cors({origin: 'https://asteroids.notaroomba.xyz'}));
-    app.use("/scores", scoresRouter);
+    app.use(cors(corsOptions));
+    app.options("*", cors(corsOptions));
+    app.use("/scores", scoresRouter, cors(corsOptions));
 
     app.use("/", async (_req: Request, res: Response) => {
       res.status(200).send("You arent supposed to be here");
-    }, cors({origin: 'https://asteroids.notaroomba.xyz'}));
+    }, cors(corsOptions));
     app.listen(port, () => {
       console.log(`Server started at http://localhost:${port}`);
     });
